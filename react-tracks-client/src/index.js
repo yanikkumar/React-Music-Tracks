5aebb9ab04d74be6afff05bcc88d147b832a9d08
Yanik Kumar <believemasters@gmail.com>
2019-07-14 21:52:44 +0530

    React-Music-Tracks
    Music Web application

---
+++ react-tracks-client/src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
+// connecting the react project with backend
+import { ApolloProvider, Query } from 'react-apollo';    // makes apollo available to all of the content using react context
+import ApolloClient, { gql } from 'apollo-boost';
+
+import Root from "./Root";
+import Auth from './components/Auth';
+
+const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql/",
+    fetchOptions: {
+        credentials: "include"
+    },
+    request: operation => {
+        const token = localStorage.getItem('authToken') || ""
+        operation.setContext({
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        })
+    },
+    clientState: {
+        defaults: {
+            isLoggedIn: !!localStorage.getItem('authToken') // convert string value to boolean by !!
+        }
+    }
+});
+
+const IS_LOGGED_IN_QUERY = gql`
+query{
+    isLoggedIn @client
+}
+`
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Query query={IS_LOGGED_IN_QUERY}>
+            {({ data }) => data.isLoggedIn ? <Root /> : <Auth />}
+        </Query>
+    </ApolloProvider>,
+    document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ read.md
@@ -0,0 +1,21 @@
+Project made using React , using number of react hooks i.e. Material UI.
+GraphQL on the client with the help of apollo client and apollo-boost
+all of that on top of a graphQL bakend that was only made wiht python Django and Graphene
+
+pipenv shell for making a virtual environment
+
+install these apps 
+pipenv install django graphene-django django-graphql-jwt django-cors-headers
+
+optional install code formatter 
+pipenv install --dev autopep8
+
+https://github.com/flavors/django-graphql-jwt
+
+https://insomnia.rest/
+
+https://github.com/reedbarger/react-tracks-client
+
+apollo graphql
+
+material UI for themes and material and styling
